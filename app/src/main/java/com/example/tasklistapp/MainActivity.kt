424c981f024b458c2d07ed48c9905faa9d56f131
Bernardo Morais <bernardojmorais@gmail.com>
2025-07-04 15:56:14 +0100

    TaskItem

--- app/src/main/java/com/example/tasklistapp/MainActivity.kt
+++ app/src/main/java/com/example/tasklistapp/MainActivity.kt
@@ -3,15 +3,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import com.example.tasklistapp.ui.theme.TaskListAppTheme
+import androidx.compose.ui.unit.sp
 
 data class Task(
     val title: String,
@@ -101,4 +104,16 @@ fun TaskListApp() {
             }
         }
     }
+}
+@Composable
+fun TaskItem(
+    task: Task,
+    onToggle: () -> Unit,
+    onRemove: () -> Unit,
+    onEdit: () -> Unit,
+    isEditing: Boolean,
+    onDetailsChange: (String) -> Unit,
+    onDoneEditing: () -> Unit
+) {
+
 }
